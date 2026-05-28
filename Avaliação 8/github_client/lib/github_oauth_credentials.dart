ea9063caff9965c8ff29a3acb19ff0019afb76ff
Liaaakkkk <alencar.lins07@aluno.ifce.edu.br>
2026-05-28 17:40:06 -0300

    parte da atividade da avaliação8

---
+++ Avaliação 8/github_client/lib/github_oauth_credentials.dart
@@ -0,0 +1,6 @@
+// TODO(CodelabUser): Create an OAuth App
+const githubClientId = 'Ov23liP6XFc3TKnWPtAO';
+const githubClientSecret = '2c2b2966e1fc1bbe393b5c00abb2e59f9456b55f';
+
+// OAuth scopes for repository and user information
+const githubScopes = ['repo', 'read:org'];
